b41913fd7d29b1afb7de4b2329fb95168c4fbe7f
isJaakko <1512803764@qq.com>
2020-09-22 00:14:23 +0800

    feat: 添加ts-react模版

--- const/repo.js
+++ const/repo.js
@@ -1,7 +1,9 @@
-const reactRepo = 'https://github.com/isJaakko/webpack-react-demo.git';
+const reactRepo = 'https://github.com/isJaakko/react-mobx-app.git';
+const tsReactRepo = 'https://github.com/isJaakko/ts-react.git';
 const koaRepo = 'https://github.com/isJaakko/ts-koa-template.git';
 
 module.exports = {
   reactRepo,
+  tsReactRepo,
   koaRepo
 };
